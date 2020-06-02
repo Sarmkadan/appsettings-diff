24d6507413981e81224bda5f0e7f9117f419d9cb
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-02 15:28:36 +0300

    polish

---
+++ README.md
@@ -0,0 +1,5 @@
+# appsettings-diff
+
+Diffs appsettings across environments and finds keys missing between them.
+
+> v0.1 in progress.
---
+++ src/AssemblyMarker.cs
@@ -0,0 +1,4 @@
+namespace AppsettingsDiff;
+
+// Placeholder so the package compiles before feature files land.
+internal static class AssemblyMarker { }
