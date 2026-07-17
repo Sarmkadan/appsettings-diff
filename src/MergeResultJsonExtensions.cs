7a5dac6ea42d68848f6863bfe46af126023a1d04
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 12:27:16 +0300

    tighten guard clauses in MergeResultJsonExtensions

--- src/MergeResultJsonExtensions.cs
+++ src/MergeResultJsonExtensions.cs
@@ -19,14 +19,11 @@ public static class MergeResultJsonExtensions
     /// <param name="value">The <see cref="MergeResult"/> to serialize.</param>
     /// <param name="indented">If <c>true</c>, the output JSON will be formatted with indentation.</param>
     /// <returns>A JSON representation of the <paramref name="value"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
     public static string ToJson(this MergeResult value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
-
-        return JsonSerializer.Serialize(
-            value,
-            indented ? new JsonSerializerOptions(_options) { WriteIndented = true } : _options);
+        return JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_options) { WriteIndented = true } : _options);
     }
 
     /// <summary>
@@ -34,8 +31,8 @@ public static class MergeResultJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string representing a <see cref="MergeResult"/>.</param>
     /// <returns>The deserialized <see cref="MergeResult"/>, or <c>null</c> if the JSON document is the literal <c>null</c>.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or an empty string.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized into a <see cref="MergeResult"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <c>null</c> or an empty string.</exception>
+    /// <exception cref="JsonException">The JSON is invalid or cannot be deserialized into a <see cref="MergeResult"/>.</exception>
     public static MergeResult? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -51,7 +48,7 @@ public static class MergeResultJsonExtensions
     /// otherwise, <c>null</c>.
     /// </param>
     /// <returns><c>true</c> if deserialization succeeded; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or an empty string.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <c>null</c> or an empty string.</exception>
     public static bool TryFromJson(string json, out MergeResult? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
