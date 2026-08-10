291ba9aac4e702baf2ae40b00c106e507c0a5d76
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-10 22:50:06 +0000

    chore: maintenance updates

--- src/SensitiveKeyDetector.cs
+++ src/SensitiveKeyDetector.cs
@@ -1,12 +1,12 @@
 using System.Text.RegularExpressions;
 
-namespace SensitiveKeyDetector
+namespace SensitiveKeyDetection
 {
     /// <summary>
     /// Detects sensitive configuration keys using a compiled regular expression.
     /// </summary>
     public class SensitiveKeyDetector
     {
-        private static readonly Regex sensitiveKeyRegex = new Regex("[^"]+", RegexOptions.Compiled);
+        private static readonly Regex sensitiveKeyRegex = new Regex("[^\"]+", RegexOptions.Compiled);
     }
 }
